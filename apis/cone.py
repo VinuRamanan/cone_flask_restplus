9e71d6a9993fab9ba23b72569a8a9f635e3ccdf2
VinuRamanan <vinufutureastronaut@gmail.com>
2020-08-02 16:16:51 +0530

    created endpoint /cone post for mock inserting

--- apis/cone.py
+++ apis/cone.py
@@ -1,10 +1,101 @@
-from flask import Flask
-from flask_restplus import Namespace, Resource, fields
+from flask import Flask, request
+from flask_restplus import Namespace, Resource, fields, reqparse
+import json
+
+
+class TimeStampInSeconds(fields.Raw):
+    def format(self, value):
+        return int(time.time())
+
 
 namespace = Namespace('cone', description='Cone Related Operations')
 
 output_model = namespace.model('Greeting', {'message': fields.String(
     required=True, description='Just a greeting')})
+
+insert_input_model = namespace.model('Cone Record',
+                                     {
+                                         'date': fields.Date(
+                                             required=True,
+                                             description='Date'
+                                         ),
+                                         'time': TimeStampInSeconds(
+                                             required=True,
+                                             description='Time starting from 1970 in seconds'
+                                         ),
+                                         'lot_number': fields.Integer(
+                                             required=True,
+                                             description='Lot Number'
+                                         ),
+                                         'yarn_type': fields.String(
+                                             required=True,
+                                             description='Yarn Type'
+                                         ),
+                                         'customer_name': fields.String(
+                                             required=True,
+                                             description='Customer Name'
+                                         ),
+                                         'lot_weight': fields.Float(
+                                             required=True,
+                                             description='Lot Weight'
+                                         ),
+                                         'yarn_count': fields.String(
+                                             required=True,
+                                             description='Yarn Count'
+                                         ),
+                                         'height': fields.Float(
+                                             required=True,
+                                             description='Height'
+                                         ),
+                                         'sample_number': fields.Integer(
+                                             required=True,
+                                             description='Sample Number'
+                                         ),
+                                         'density': fields.Float(
+                                             required=True,
+                                             description='Density'
+                                         ),
+                                         'spindle_number': fields.Float(
+                                             required=True,
+                                             description='Spindle Number'
+                                         ),
+                                         'error_type': fields.String(
+                                             required=True,
+                                             description='Error Type'
+                                         ),
+                                         'laser_raw_output': fields.Float(
+                                             required=True,
+                                             description='Laser Raw Output'
+                                         ),
+                                         'outer_radius': fields.Float(
+                                             required=True,
+                                             description='Outer Radius'
+                                         ),
+                                         'volume': fields.Float(
+                                             required=True,
+                                             description='Volume'
+                                         ),
+                                         'weight_raw_output': fields.Float(
+                                             required=True,
+                                             description='Weight Raw Output'
+                                         ),
+                                         'mass': fields.Float(
+                                             required=True,
+                                             description='Mass'
+                                         ),
+                                         'barcode_raw_input': fields.Float(
+                                             required=True,
+                                             description='Barcode Raw Input'
+                                         ),
+                                     })
+
+insert_output_model = namespace.model('Insertion Response',
+                                      {
+                                          'response': fields.String(
+                                              required=True,
+                                              description='Response for insertion of a cone'
+                                          )
+                                      })
 
 
 @namespace.route('')
@@ -12,3 +103,9 @@ class Cone(Resource):
     @namespace.marshal_list_with(output_model)
     def get(self):
         return {'message': 'This is the cone API'}
+
+    @namespace.expect(insert_input_model)
+    def post(self):
+        data = json.loads(request.data)
+        response = {'response': str(data)}
+        return response
