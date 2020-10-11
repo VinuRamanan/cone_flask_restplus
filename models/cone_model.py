54917933b0359be9c3ca49fffb32d624113f0a2d
VinuRamanan <vinufutureastronaut@gmail.com>
2020-10-11 19:59:48 +0530

    added mocks

--- models/cone_model.py
+++ models/cone_model.py
@@ -1,5 +1,6 @@
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy import Column, Integer, String, Float
+import datetime as dt
+from sqlalchemy import Column, Integer, String, Float, Date
 import time
 from config import db
 
@@ -10,21 +11,24 @@ class ConeRecord(db.Model):
     id = Column(Integer, primary_key=True)
     time_stamp = Column(Integer, default=int(
         time.time()), onupdate=int(time.time()))
+    date = Column(Date, default=dt.datetime.now, onupdate=dt.datetime.now)
     lot_number = Column(String)
     yarn_type = Column(String)
     customer_name = Column(String)
     lot_weight = Column(Float)
     yarn_count = Column(String)
-    lot_height = Column(Float)
+    minimum_lot_height = Column(Float)
+    maximum_lot_height = Column(Float)
     sample_number = Column(Integer)
     density = Column(Float)
     spindle_number = Column(Float)
     error_type = Column(String)
     laser_raw_output = Column(Float)
-    outer_radius = Column(Float)
+    outer_diameter = Column(Float)
     volume = Column(Float)
     weight_raw_output = Column(Float)
     mass = Column(Float)
+    weight = Column(Float)
     barcode_raw_input = Column(Float)
 
     def __repr__(self):
