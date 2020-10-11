3447459f576ba24553f9fc114de46b3e0d6bbb0e
VinuRamanan <vinufutureastronaut@gmail.com>
2020-10-11 20:18:35 +0530

    com

--- apis/__init__.py
+++ apis/__init__.py
@@ -2,7 +2,6 @@ from flask_restplus import Api
 from .cone import namespace as namespace_cone
 from .generator import namespace as namespace_generate
 from .density import namespace as namespace_density
-from .com import namespace as namespace_com
 
 api = Api(
     title='Wall of APIs',
@@ -13,4 +12,3 @@ api = Api(
 api.add_namespace(namespace_cone)
 api.add_namespace(namespace_generate)
 api.add_namespace(namespace_density)
-api.add_namespace(namespace_com)
