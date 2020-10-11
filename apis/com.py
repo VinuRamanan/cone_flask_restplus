dfc3514e93b59c22f81c647db9fac323b0a49bf4
VinuRamanan <vinufutureastronaut@gmail.com>
2020-10-11 20:14:16 +0530

    added namespace

---
+++ apis/com.py
@@ -0,0 +1,11 @@
+from flask_restplus import Resource, Namespace
+from resources.ports import serial_ports
+
+namespace = Namespace('COMPorts', description='Port related Operations')
+
+
+@namespace.route('')
+class Ports(Resource):
+    @namespace.doc('Get all the active ports')
+    def get(self):
+        return {'ports': [{'name': port[:3], 'value':port[3:]} for port in serial_ports()]}
